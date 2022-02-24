35723e1d88d44b42a338f350fa1cab4932c28781
Mekarosi <mekarosi_assa@yahoo.com>
2022-02-24 06:25:46 -0800

    Add google map

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -11,8 +11,10 @@ export default makeStyles((theme) => ({
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': { backgroundColor: alpha(theme.palette.common.white, 0.25) },
-    marginRight: theme.spacing(2),
+    '&:hover': { 
+        backgroundColor: alpha(theme.palette.common.white, 0.25) 
+    },
+    // marginRight: theme.spacing(2),
     marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
